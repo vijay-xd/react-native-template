2db2749b18a5b807560dffb0f5e87cb8565fc9b6
yugha13 <yughas83@gmail.com>
2026-01-10 16:32:22 +0530

    nativewind intigrated

--- components/ScreenContent.tsx
+++ components/ScreenContent.tsx
@@ -11,16 +11,11 @@ type ScreenContentProps = {
 
 export const ScreenContent = ({ title, path, children }: ScreenContentProps) => {
   return (
-    <View className={styles.container}>
-      <Text className={styles.title}>{title}</Text>
-      <View className={styles.separator} />
+    <View className="items-center flex-1 justify-center bg-white">
+      <Text className="text-xl font-bold">{title}</Text>
+      <View className="h-[1px] my-7 w-4/5 bg-gray-200" />
       <EditScreenInfo path={path} />
       {children}
     </View>
   );
-};
-const styles = {
-  container: `items-center flex-1 justify-center bg-white`,
-  separator: `h-[1px] my-7 w-4/5 bg-gray-200`,
-  title: `text-xl font-bold`,
 };
